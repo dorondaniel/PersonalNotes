599163eb2446392807ac2b1343d8da139dc65595
Doron.N.C.Daniel <idorondaniel@gmail.com>
2023-11-27 15:01:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/doron/personalnotes/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.doron.personalnotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.widget.ProgressBar
+
+class SplashActivity : AppCompatActivity() {
+    var prgs_bar : ProgressBar? = null
+    var i = 0
+    var handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        prgs_bar = findViewById<ProgressBar>(R.id.progess)
+
+        handler.postDelayed(Runnable(){
+                               startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}
+
